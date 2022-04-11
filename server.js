a3abc164a64173ccd4d589bd3dcaf7e5adac8ae2
akayode <akayode@live.unc.edu>
2022-04-11 16:02:15 -0400

    PLEASE WORK!

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
     referer: req.headers['referer'],
     useragent: req.headers['user-agent']
   }
-  const stmt = logdb.prepare('INSERT INTO accesslog (remote_addr, remote_user, time, method, url, protocol, httpversion, secure, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?, ?)')
+  const stmt = logdb.prepare('INSERT INTO accesslog (remote_addr, remote_user, time, method, url, protocol, http_version, secure, status, referer, user_agent) VALUES (?,?,?,?,?,?,?,?,?,?, ?)')
   stmt.run(logdata.remoteaddr, String(logdata.remoteuser), logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, String(logdata.secure), logdata.status, logdata.referer, logdata.useragent);
   next();
 });
