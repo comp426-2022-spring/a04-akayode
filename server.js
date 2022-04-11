1bd39584efe93f2c03a126e84084edfeb5d105e9
akayode <akayode@live.unc.edu>
2022-04-11 13:19:39 -0400

    Changed help

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ if (argv.help) {
   console.log("server.js [options]")
   console.log("  --por		Set the port number for the server to listen on. Must be an integer")
   console.log("              	between 1 and 65535.")
+  console.log()
   console.log("  --debug	If set to `true`, creates endlpoints /app/log/access/ which returns")
   console.log("              	a JSON access log from the database and /app/error which throws ")
   console.log('              	an error with the message "Error test successful." Defaults to ')
@@ -28,6 +29,39 @@ if (argv.help) {
   process.exit()
 }
 
+app.use((req, res, next) => {
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  }
+  const stmt = logdb.prepare('INSERT INTO accesslog (remote_addr, remote_user, time, method, url, protocol, http_version, status, referer, user_agent) VALUES (?,?,?,?,?,?,?,?,?,')
+  stmt.run(logdata.remoteaddr, String(logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent));
+  next();
+})
+
+if (argv.debug == true) {
+  app.get('/app/log/access', (req, res) => {
+    try {
+      const stmt = logdb.prepare('SELECT * FROM accesslog').all();
+      res.status(200).json(stmt);
+    } catch  {
+      console.error(e)
+    }
+  })
+  app.get('/app/error', (req, res) => {
+    throw new Error("Error test successful.")
+  })
+}
+
+
 const port = args.port || process.env.PORT || 5000
 
 app.use(express.urlencoded({extend: true}));
@@ -36,6 +70,13 @@ app.use(express.json());
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
+
+if (argv.log == true) {
+  const accesslog = fs.createWriteStream('./access.log', {flags: 'a'});
+  app.use(morgan("tiny", {stream: accesslog}));
+} else {
+  app.use(morgan("tiny"))
+}
 
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
@@ -109,43 +150,7 @@ app.use(function(req, res){
     res.type("text/plain")
 });
 
-if (argv.log == true) {
-  const accesslog = fs.createWriteStream('./access.log', {flags: 'a'});
-  app.use(morgan("tiny", {stream: accesslog}));
-} else {
-  app.use(morgan("tiny"))
-}
 
-app.use((req, res, next) => {
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
-  }
-  const stmt = logdb.prepare('INSERT INTO accesslog (remote_addr, remote_user, time, method, url, protocol, http_version, status, referer, user_agent) VALUES (?,?,?,?,?,?,?,?,?,')
-  stmt.run(logdata.remoteaddr, String(logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent));
-  next();
-})
 
-if (argv.debug == true) {
-  app.get('/app/log/access', (req, res) => {
-    try {
-      const stmt = logdb.prepare('SELECT * FROM accesslog').all();
-      res.status(200).json(stmt);
-    } catch  {
-      console.error(e)
-    }
-  })
-  app.get('/app/error', (req, res) => {
-    throw new Error("Error test successful.")
-  })
-}
 
 
